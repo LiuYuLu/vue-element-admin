611db84884f07885dac2f07b5b4671def67611f3
JadeLiu <liuyulu2@vip.qq.com>
2017-07-03 16:17:00 +0800

    Update SidebarItem.vue
    只要item.children为true就行，否则route没有配置child的时候会出错。

--- src/views/layout/SidebarItem.vue
+++ src/views/layout/SidebarItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <template v-for="item in routes">
-            <router-link v-if="!item.hidden&&item.noDropdown&&item.children.length>0" :to="item.path+'/'+item.children[0].path">
+            <router-link v-if="!item.hidden&&item.noDropdown&&item.children" :to="item.path+'/'+item.children[0].path">
                 <el-menu-item :index="item.path+'/'+item.children[0].path">
                     <wscn-icon-svg v-if='item.icon' :icon-class="item.icon" /> {{item.children[0].name}}
                 </el-menu-item>
@@ -11,7 +11,7 @@
                     <wscn-icon-svg v-if='item.icon' :icon-class="item.icon" /> {{item.name}}
                 </template>
                 <template v-for="child in item.children" v-if='!child.hidden'>
-                    <sidebar-item class='menu-indent' v-if='child.children&&child.children.length>0' :routes='[child]'> </sidebar-item>
+                    <sidebar-item class='menu-indent' v-if='child.children&&child.children' :routes='[child]'> </sidebar-item>
                     <router-link v-else class="menu-indent" :to="item.path+'/'+child.path">
                         <el-menu-item :index="item.path+'/'+child.path">
                             {{child.name}}
